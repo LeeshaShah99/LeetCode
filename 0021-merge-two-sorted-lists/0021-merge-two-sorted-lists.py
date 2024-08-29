cd99dca139a173339e5e58a02330583284362201
LeeshaShah99 <125219364+LeeshaShah99@users.noreply.github.com>
2024-08-28 21:20:01 -0400

    Time: 39 ms (50.47%), Space: 16.6 MB (34.04%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,25 +5,37 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        prehead = ListNode(-1)
-        curr = prehead
         
-        while(list1 != None and list2 != None):
-            if(list1.val < list2.val):
-                curr.next = list1
-                list1 = list1.next
-                
-            else:
-                curr.next = list2
-                list2 = list2.next 
-                
-            curr = curr.next
-            
-        if(list1 != None):
-            curr.next = list1
+        
+        if list1 == None:
+            return list2
+        elif list2 == None:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next , list2)
+            return list1
         else:
-            curr.next = list2
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+#         prehead = ListNode(-1)
+#         curr = prehead
+        
+#         while(list1 != None and list2 != None):
+#             if(list1.val < list2.val):
+#                 curr.next = list1
+#                 list1 = list1.next
                 
-        return prehead.next
+#             else:
+#                 curr.next = list2
+#                 list2 = list2.next 
+                
+#             curr = curr.next
+            
+#         if(list1 != None):
+#             curr.next = list1
+#         else:
+#             curr.next = list2
+                
+#         return prehead.next
                 
         
