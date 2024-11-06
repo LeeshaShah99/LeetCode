074637917f672b76b45278fe517c5182c2b8d7b5
LeeshaShah99 <125219364+LeeshaShah99@users.noreply.github.com>
2024-11-06 03:30:27 -0500

    Time: 350 ms (76.18%), Space: 0B (100.00%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,10 +1,8 @@
 SELECT e.employee_id
 FROM Employees e
+left join Employees m
+on e.manager_id = m.employee_id
 WHERE e.salary < 30000
   AND e.manager_id IS NOT NULL
-  AND NOT EXISTS (
-    SELECT 1
-    FROM Employees m
-    WHERE m.employee_id = e.manager_id
-  )
+  AND m.employee_id IS NULL
 ORDER BY e.employee_id;
